0443d357dd1172ba9b7444647d592fb4d14bdd12
Thomas Hupkens <thomas@hupkens.be>
2014-12-18 12:04:22 +0100

    Add permissions

---
+++ src/Kowali/Support/Facades/User.php
@@ -0,0 +1,17 @@
+<?php namespace Kowali\Support\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Locale extends Facade {
+
+	/**
+     * Return the name of the IOC biding behind the facade
+	 *
+	 * @return string
+	 */
+    protected static function getFacadeAccessor()
+    {
+        return 'kowali.permissions.user';
+    }
+
+}
